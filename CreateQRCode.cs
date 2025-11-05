672a1796e330c9e314c68ba46e89e14b4f255d67
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2025-11-05 13:55:10 +0100

    Update QR code generation to use hex colors

--- CreateQRCode.cs
+++ CreateQRCode.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Mcp;
 using Microsoft.Extensions.Logging;
 using QRCoder;
-using System.Drawing; 
 
 namespace QRCodeFunction
 {
@@ -35,7 +34,7 @@ namespace QRCodeFunction
 
         [Function("createqrcodehtml")]
         public string RunQrCodeHtml(
-            [McpToolTrigger("createqrcodehtml", "Generates a QR code in HTML format from the provided text.")]
+            [McpToolTrigger("createqrcodehtml", "Generates a QR code in base64 jpg format from the provided text.")]
             ToolInvocationContext context,
             [McpToolProperty("text", "The text which should be encoded.", isRequired: true)]
             string text)
@@ -48,7 +47,7 @@ namespace QRCodeFunction
                 using (Base64QRCode qrCode = new Base64QRCode(qrCodeData))
                 {
                     var imgType = Base64QRCode.ImageType.Jpeg;
-                    string qrCodeImageAsBase64 = qrCode.GetGraphic(20, Color.Black, Color.White, true, imgType);
+                    string qrCodeImageAsBase64 = qrCode.GetGraphic(20, "#000000", "#FFFFFF", true, imgType);
                     var htmlPictureTag =  $"<img alt=\"Embedded QR Code\" src=\"data:image/{imgType.ToString().ToLower()};base64,{qrCodeImageAsBase64}\" />";
                     return htmlPictureTag;
                 }
